1e111d218c0c40de8773abf8ba191225bf37a9fd
Takumi Araki <apndx19@gmail.com>
2022-05-24 11:09:28 +0900

    DBManager 作成完了

--- Project/src/main/java/base/DBManager.java
+++ Project/src/main/java/base/DBManager.java
@@ -1,5 +1,35 @@
 package base;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBManager {
+  private static final String URL = "jdbc:mysql://localhost/";
+  private static final String DB_NAME = "usermanagement";
+  private static final String PARAMETERS = "?useUnicode=true&characterEncoding=utf8";
+  private static final String USER = "root";
+  private static final String PASS = "password";
+  
+  public static Connection getConnection() {
+
+    Connection con = null;
+
+    try {
+      // jdbcドライバへアクセス
+      Class.forName("com.mysql.jdbc.Driver");
+
+      con = DriverManager.getConnection(URL + DB_NAME + PARAMETERS, USER, PASS);
+      System.out.println("DB Conncected");
+      return con;
+
+    } catch (ClassNotFoundException | SQLException e) {
+      throw new IllegalMonitorStateException();
+    }
+
+
+  }
+
+
 
 }
