55743a2177ee605e5c19d86b4d82b199c48fcee6
Takumi Araki <apndx19@gmail.com>
2022-05-24 11:20:43 +0900

    ExpenseDataBeans作成完了

--- Project/src/main/java/beans/ExpenseDataBeans.java
+++ Project/src/main/java/beans/ExpenseDataBeans.java
@@ -1,5 +1,97 @@
 package beans;
 
-public class ExpenseDataBeans {
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class ExpenseDataBeans implements Serializable {
+  private int id;
+  private int userId;
+  private int categoryId;
+  private String name;
+  private int price;
+  private Date expenseDate;
+  private String note;
+  private Timestamp createDate;
+  private Timestamp updateDate;
+
+  public ExpenseDataBeans() {}
+
+  // getterとsetter
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(int categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public Date getExpenseDate() {
+    return expenseDate;
+  }
+
+  public void setExpenseDate(Date expenseDate) {
+    this.expenseDate = expenseDate;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+  public Timestamp getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Timestamp createDate) {
+    this.createDate = createDate;
+  }
+
+  public Timestamp getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(Timestamp updateDate) {
+    this.updateDate = updateDate;
+  }
+
+
+
+
 
 }
--- Project/src/main/java/dao/ExpenseDAO.java
+++ Project/src/main/java/dao/ExpenseDAO.java
@@ -2,4 +2,8 @@ package dao;
 
 public class ExpenseDAO {
 
+  //とりあえずメソッドを作りたい、でも先にBeansを作成しないと、クラスを定義できない
+
+  
+  
 }
